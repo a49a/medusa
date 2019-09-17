d5b16da8e0a4c17ba2dc4f7beb06cda23c594229
wang <rsl4@foxmail.com>
2019-09-17 23:22:51 +0800

    add scrapy demo

--- demo.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-data = [1, 2, -1, -2]
-
-tensor = torch.FloatTensor(data)
-
-print(tensor)
---
+++ demo/demo.py
@@ -0,0 +1,15 @@
+
+data = [1, 2, -1, -2]
+
+x = data[:8]
+i = range(0, 5)
+l = list(i)
+l2 = list(i)
+print(l2)
+def foo(i):
+    l = list(i)
+    print(l)
+
+
+print("Vector(%r)" % x)
+
